9fd5cc31f76172bac464b276bc48d6310cc3a99e
pyx26266 <amankumar.msjps@gmail.com>
2019-02-16 18:48:12 +0530

    client and server folder created

---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Header msg="Prescription Reader"/>
+    <upload-file />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import UploadFile from '@/components/UploadFile'
+
+export default {
+  name: 'home',
+  components: {
+    Header,
+    UploadFile
+  }
+}
+</script>
